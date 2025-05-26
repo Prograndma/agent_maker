01f35fe5e7a6adcbca48a119721f4792599c6377
Prograndma <thomasvcole@gmail.com>
2025-05-25 21:58:30 -0600

    A list of perhaps strange wikipedia articles for the bot to use.

---
+++ sms_agent/fun_topic.py
@@ -0,0 +1,58 @@
+import random
+from smolagents import Tool
+
+
+class GetFunTopic(Tool):
+    name = "get_fun_topic"
+    description = """
+        This tool provides a fun topic from a list of good choices!
+        """
+    inputs = {
+
+    }
+    output_type = "string"
+
+    @staticmethod
+    def forward():
+        topics = ["Pizza farm"
+                  "Spite house"
+                  "list of tautological place names",
+                  "Valeriepieris circle",
+                  "Fanta Orange Soda",
+                  "Cheese",
+                  "modern Toilet Restaurant",
+                  "Neutrality Monument",
+                  "616 (number)",
+                  "65537-gon",
+                  "All horses are the same color",
+                  "Almost everywhere",
+                  "Hairy ball theorem",
+                  "Cinnamon Roll Day",
+                  "Thanksgivukkah",
+                  "Undecimber",
+                  "Bouba/kiki effect",
+                  "Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo",
+                  "Covfefe",
+                  "Duck test",
+                  "Ghost characters",
+                  "Inherently funny word",
+                  "James while John had had had had had had had had had had had a better effect on the teacher",
+                  "Lion-Eating Poet in the Stone Den",
+                  "List of English words without rhymes",
+                  "List of English words containing Q not followed by U",
+                  "\"My postillion has been struck by lightning\"",
+                  "Archaeoacoustics",
+                  "Buttered cat paradox",
+                  "Kardashian Index",
+                  "John G. Trump",
+                  "Furry's theorem",
+                  "Mpemba effect",
+                  "Shower-curtain effect",
+                  "Continental drip",
+                  "Mothers against decapentaplegic",
+                  "Death from laughter",
+                  "Execution by elephant",
+                  "What Is It Like to Be a Bat?",
+                  ]
+
+        return random.choice(topics)
