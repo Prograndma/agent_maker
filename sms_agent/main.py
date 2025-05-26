72f7485b6232df6c78c545f1f9052e9e6eddb367
Prograndma <thomasvcole@gmail.com>
2025-05-25 23:04:32 -0600

    adding info prints.

--- sms_agent/main.py
+++ sms_agent/main.py
@@ -10,24 +10,28 @@ def big_print(message):
     print("###############################")
     print(message)
     print("###############################")
-
+print("LOGGING IN")
 login(LOGIN_TOKEN)
+print("Getting model")
 model = TransformersModel(model_id="meta-llama/Llama-3.2-3B-Instruct",
                           device_map="auto",
                           max_new_tokens=256)
+print("FINISHED!")
 search_tool = WikipediaSearchTool(
     user_agent="MyFunBot (tocm43841@gmail.com)",
     language="en",
     content_type="summary",
     extract_format="WIKI",
 )
+print("Making agent")
 agent = CodeAgent(tools=[search_tool, TextMatt(), GetFunTopic(), CountLettersInWord(),],
                   model=model,
-                  max_steps=5,
+                  max_steps=3,
                   verbosity_level=2)
 
-agent_input = ("Hey! Could you help me? I want to send matt a fun snippet or factoid about a fun topic! "
-               "Even if the topic is a little weird. Please use wikipedia to learn more about a fun topic.")
+agent_input = ("you're a tool following agent, so why don't you get a fun topic "
+               "and then get the wikipedia summary about it in step one. In step two summarize"
+               "what you learned in a message to matt! ")
 big_print(f"SMS Agent Sex: {agent.run(agent_input)}")
 exit()
 while True:
