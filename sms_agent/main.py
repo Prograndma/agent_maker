72105469fc4ffd9688645a7c9aa755d84616dd2c
Prograndma <thomasvcole@gmail.com>
2025-05-25 23:41:57 -0600

    Typo, changing to sez

--- sms_agent/main.py
+++ sms_agent/main.py
@@ -14,8 +14,17 @@ print("LOGGING IN")
 login(LOGIN_TOKEN)
 print("Getting model")
 model = TransformersModel(model_id="meta-llama/Llama-3.2-3B-Instruct",
-                          device_map="auto",
+                          device_map="cpu",
                           max_new_tokens=256)
+#
+# model = TransformersModel(model_id="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
+#                           device_map="auto",
+#                           max_new_tokens=256)
+
+# model = TransformersModel(model_id="deepseek-ai/deepseek-coder-1.3b-instruct",
+#                           device_map="auto",
+#                           max_new_tokens=256)
+
 print("FINISHED!")
 search_tool = WikipediaSearchTool(
     user_agent="MyFunBot (tocm43841@gmail.com)",
@@ -26,14 +35,11 @@ search_tool = WikipediaSearchTool(
 print("Making agent")
 agent = CodeAgent(tools=[search_tool, TextMatt(), GetFunTopic(), CountLettersInWord(),],
                   model=model,
-                  max_steps=3,
+                  max_steps=6,
                   verbosity_level=2)
 
-agent_input = ("you're a tool following agent, so why don't you get a fun topic "
-               "and then get the wikipedia summary about it in step one. In step two summarize"
-               "what you learned in a message to matt! ")
-big_print(f"SMS Agent Sex: {agent.run(agent_input)}")
-exit()
+agent_input = ("Find a fun topic, learn about it and then send a message about it to Matt.")
+big_print(f"SMS Agent Sez: {agent.run(agent_input)}")
 while True:
     print("(Your turn to talk.)")
     agent_input = input()
